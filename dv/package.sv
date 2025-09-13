4f146c168368ccfd183d302fb38efa284b097994
Abdelrahman <Abdelrahman.Yassien11@gmail.com>
2025-09-13 22:17:15 +0300

    updating it

--- dv/package.sv
+++ dv/package.sv
@@ -1,11 +1,17 @@
-`include "seq_item.sv"
-`include "ral_pkg.sv"
-`include "ral2axi_adapter.sv"
+package pkg;
+    import uvm_pkg::*;
+    `include "uvm_macros.svh"
+  
+    `include "seq_item.sv"
+    `include "ral_pkg.sv"
+    `include "ral2axi_adapter.sv"
 
-`include "base_seq.sv"
-`include "sequencer.sv"
-`include "driver.sv"
-`include "monitor.sv"
-`include "agent.sv"
-`include "env.sv"
-
+    `include "base_seq.sv"
+    `include "sequencer.sv"
+    `include "driver.sv"
+    `include "monitor.sv"
+    `include "agent.sv"
+    `include "env.sv"
+    `include "base_test.sv"
+    `include "interface.sv"
+endpackage : pkg
